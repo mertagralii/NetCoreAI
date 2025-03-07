12ca28251d20498725375b9973566c5f778aa150
Mert Ağralı <my_mert07hotmail.com>
2025-03-07 16:49:53 +0300

    Notlar Hazırlandı.

--- NetCoreAI.Project01.ApiDemo/Controllers/CustomersController.cs
+++ NetCoreAI.Project01.ApiDemo/Controllers/CustomersController.cs
@@ -16,23 +16,23 @@ namespace NetCoreAI.Project01.ApiDemo.Controllers
             _context = context;
         }
 
-        [HttpGet("CustomerList")]
-        public IActionResult CustomersList() 
+        [HttpGet] // GET ==> Veri çekme işlemi yapacağımız zaman kullanıyoruz.
+        public IActionResult CustomersList()
         {
-           var customersList = _context.Customers.ToList();
+            var customersList = _context.Customers.ToList();
             return Ok(customersList);
         }
 
-        [HttpPost("CreateCustomers")]  
-        public IActionResult CreateCustomers(Customer customer) 
+        [HttpPost("CreateCustomers")]  // POST ==> Ekleme işlemi yapacağımız zaman kullanıyoruz. 
+        public IActionResult CreateCustomers(Customer customer)
         {
-             _context.Customers.Add(customer);
+            _context.Customers.Add(customer);
             _context.SaveChanges();
             return Ok("Müşteri Ekleme İşlemi Gerçekleştirildi.");
         }
 
-        [HttpDelete("DeleteCustomers")]
-        public IActionResult DeleteCustomers(int Id) 
+        [HttpDelete("DeleteCustomers")] // Delete ==> Silme işlemi yapacağımız zaman kullanıyoruz.
+        public IActionResult DeleteCustomers(int Id)
         {
             var deleteCustomers = _context.Customers.Find(Id);
 
@@ -41,22 +41,26 @@ namespace NetCoreAI.Project01.ApiDemo.Controllers
             return Ok("Müşteri Silme İşlemi Gerçekleştirildi.");
         }
 
-        [HttpGet("GetCustomer")]
-        public IActionResult GetCustomer(int Id) 
+        [HttpGet("GetCustomer")] // ÖNEMLİ ==> Yukarda daha önceden get kullandığımız için onu da bununla ayırabilmek için bu sefer geti böyle kullandık. İsimlendirdik.
+        public IActionResult GetCustomer(int Id)
         {
             var getCustomer = _context.Customers.Find(Id);
             return Ok(getCustomer);
         }
 
-        [HttpPut("UpdateCustomers")]
-        public IActionResult UpdateCustomer(Customer customer) 
+        [HttpPut("UpdateCustomers")] // PUT ==> Güncelleme yapacağımız zaman kullanıyoruz.
+        public IActionResult UpdateCustomer(Customer customer)
         {
             _context.Customers.Update(customer);
             _context.SaveChanges();
             return Ok("Müşteri Güncelleme İşlemi Gerçekleştirildi.");
         }
 
-
+        // Dip NOT : 
+        // Silme İşlemi Yapacağımız zaman HttpDelete, Güncelleme İşlemi yapacağımız zaman HttpPUT, Veri çekeceğimiz zaman HTTPGet, Veri EKleyeceğimiz zaman HttpPost kullanıyoruz.
+        // Api'de daha öncesinde bir yerde HTTPGET kullanmışsak, diğer yerlerde de HTTPGET kullanmamız gerekiyor. Aynı şekilde diğerleri içinde geçerlidir.
+        // Bunun sebebi, Api'nin çalışma mantığıdır.
+        // Eğer başka bir yerde HTTPGET kullanmışsak, diğer yerlerde de kullanmamız gerekiyorsa o zaman [HttpGet("İsim")] yazmamız gerekiyor.Böylelikle metotları ayırabiliriz.
 
     }
 }
